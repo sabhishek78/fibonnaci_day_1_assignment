c10375b835f8b8584308c73526a39e20898d088e
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-01 08:16:40 +0530

    Fibonacci Series finished

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,15 @@
+//import 'dart:io';
 void main() {
-  for(var temp, int i = 0,int j = 1,int n=1; n<=30; temp = i, i = j, j = i + temp) {
-    print('${j}');
+  int i=0;
+  int j=1;
+  int n=1;
+  int temp;
+  print(i);
+  while(n<=30 ) {
+    temp = i;
+    i = j;
+    j = i + temp;
+    print( j );
     n++;
   }
 }
